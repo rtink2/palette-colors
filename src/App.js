bf2c077dc019d436c28b0f396006648b8d256973
Rhonda <rtinkham0408@gmail.com>
2020-04-28 22:17:38 -0400

    add palette list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
-  );
+import Palette from './Palette';
+import PaletteList from './PaletteList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.findPalette = this.findPalette.bind(this);
+  }
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    );
+  }
 }
 
 export default App;
